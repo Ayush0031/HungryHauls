522d3e034cb12b7ddbb4f18af7be8fb163b4b1b5
AYUSH <89582411+Ayush0031@users.noreply.github.com>
2024-04-23 23:48:43 +0530

    added logos carousel

--- mernapp/src/screens/Home.js
+++ mernapp/src/screens/Home.js
@@ -2,11 +2,13 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Card from "../components/Card";
+import Carousel from "../components/Carousel";
 
 export default function Home() {
   return (
     <div>
       <div><Navbar /></div>
+      <div><Carousel/></div>
       <div><Card/></div>
       <div><Footer /></div>
         
